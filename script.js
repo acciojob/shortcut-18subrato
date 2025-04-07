49852d88a5d4d9f1386c914994369357ae017cf3
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-04-07 13:10:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function shortcut(s1, s2) {
-  // your code here
+	let newStr = '';
+	s1 = s1.trim();
+	s2 = s2.trim();
+	if(s1 == '' && s2 ==''){
+		return newStr;
+	} else if(s1 !== '' && s2 !==''){
+		newStr += s1.charAt(0) + s2.charAt(0);
+	} else if(s1 !== '' && s2 ===''){
+		return newStr;
+	} else {
+		return newStr
+	}
+
+	return newStr;
 }
 
 // Do not change the code below.
